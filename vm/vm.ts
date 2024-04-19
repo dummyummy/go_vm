969e2992b41a234b2c62b11ada2f50e5b3cca021
Lan-Jingbo <lanjingbo114@163.com>
2024-04-19 09:39:41 +0800

    Update vm.ts

--- vm/vm.ts
+++ vm/vm.ts
@@ -51,132 +51,132 @@ function isCompiledChannel(pos: any): boolean {
 }
 
 /* ================ COMPILER ================ */
-// let INSTRUCTIONS: any[] = [];
-// let wc: number = 0;
-//
-// function compile_component(component: any, compile_environment: any[]): void {
-//     if (component.tag === "lit") {
-//         INSTRUCTIONS[wc++] = { tag: "LDC", val: component.val };
-//     } else if (component.tag === "binop") {
-//         compile_component(component.frst, compile_environment);
-//         compile_component(component.scnd, compile_environment);
-//         INSTRUCTIONS[wc++] = { tag: "BINOP", sym: component.sym };
-//     } else if (component.tag === "unop") {
-//         compile_component(component.frst, compile_environment);
-//         INSTRUCTIONS[wc++] = { tag: "UNOP", sym: component.sym };
-//     } else if (component.tag === "seq") {
-//         const sequence = component.stmts;
-//         if (sequence.length === 0) {
-//             INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
-//         }
-//         let frst = true;
-//         for (let seq_part of sequence) {
-//             frst ? frst = false : INSTRUCTIONS[wc++] = { tag: "POP" };
-//             compile_component(seq_part, compile_environment);
-//         }
-//     } else if (component.tag === "nam") {
-//         INSTRUCTIONS[wc++] = {
-//             tag: "LD",
-//             sym: component.sym,
-//             pos: compile_time_environment_position(compile_environment, component.sym)
-//         };
-//     } else if (component.tag === "assmt") {
-//         compile_component(component.right, compile_environment);
-//         INSTRUCTIONS[wc++] = {
-//             tag: "ASSIGN",
-//             pos: compile_time_environment_position(compile_environment, component.left.sym)
-//         };
-//     } else if (component.tag === "cond"){
-//         compile_component(component.pred, compile_environment);
-//         const jump_on_false_instr: { tag: string, addr?: number } = { tag: "JOF" };
-//         INSTRUCTIONS[wc++] = jump_on_false_instr;
-//         const alternative_address = wc;
-//         compile_component(component.cons, compile_environment);
-//         const goto_instr: { tag: string, addr?: number } = { tag: "GOTO" };
-//         INSTRUCTIONS[wc++] = goto_instr;
-//         compile_component(component.alt, compile_environment);
-//         jump_on_false_instr.addr = alternative_address;
-//         goto_instr.addr = wc;
-//     } else if (component.tag === "app") { // Changed from 'args' to 'arguments' to match parser
-//         compile_component(component.fun, compile_environment);
-//         const args = component.arguments;
-//         for (let arg of args) {
-//             compile_component(arg, compile_environment);
-//         }
-//         INSTRUCTIONS[wc++] = { tag: "CALL", arity: args.length };
-//     } else if (component.tag === "blk") {
-//         const locals = scan(component.body);
-//         INSTRUCTIONS[wc++] = { tag: "ENTER_SCOPE", num: locals.length, syms: locals };
-//         compile_component(component.body, compile_time_environment_extend(locals, compile_environment));
-//         INSTRUCTIONS[wc++] = { tag: "EXIT_SCOPE" };
-//     } else if (component.tag === "const") {
-//         const pos = compile_time_environment_position(compile_environment, component.sym);
-//         if (is_null(component.expr)) {
-//             INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
-//         } else {
-//             compile_component(component.expr, compile_environment);
-//         }
-//         INSTRUCTIONS[wc++] = {
-//             tag: "ASSIGN",
-//             pos: pos
-//         };
-//     } else if (component.tag === "var") {
-//         const pos = compile_time_environment_position(compile_environment, component.sym);
-//         if (is_null(component.expr)) {
-//             INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
-//         } else {
-//             compile_component(component.expr, compile_environment);
-//         }
-//         INSTRUCTIONS[wc++] = {
-//             tag: "ASSIGN",
-//             pos: pos
-//         };
-//     } else if (component.tag === "ReturnStatement") { // Changed from 'ret' and 'component.argument' to match parser
-//         compile_component(component.argument, compile_environment);
-//         INSTRUCTIONS[wc++] = { tag: "RESET" };
-//     } else if (component.tag === "fun") {
-//         // Rewrite as a const declaration to a lambda function
-//         compile_component({
-//             tag: "const",
-//             sym: component.sym,
-//             expr: {
-//                 tag: "lam",
-//                 prms: component.prms,
-//                 arity: component.prms.length,
-//                 body: component.body
-//             }
-//         }, compile_environment);
-//     } else if (component.tag === "lam") {
-//         INSTRUCTIONS[wc++] = { tag: "LDF", arity: component.arity, addr: wc + 1 };
-//         const goto_instr: { tag: string, addr?: number } = { tag: "GOTO" };
-//         INSTRUCTIONS[wc++] = goto_instr;
-//         compile_component(component.body, compile_time_environment_extend(component.prms, compile_environment));
-//         INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
-//         INSTRUCTIONS[wc++] = { tag: "RESET" };
-//         goto_instr.addr = wc;
-//     } else if (component.tag === "goroutine") {
-//         // Compile application call
-//         compile_component(component.function, compile_environment);
-//         // Change "CALL" to "GOCALL"
-//         INSTRUCTIONS[wc - 1] = { tag: "GOCALL", arity: INSTRUCTIONS[wc - 1].arity };
-//         INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
-//     } else if (component.tag === "MakeChannel") {
-//         INSTRUCTIONS[wc++] = { tag: "CREATE_CHAN" };
-//     } else if (component.tag === "Arrow") {
-//         compile_component(component.right, compile_environment);
-//         INSTRUCTIONS[wc++] = {
-//             tag: "USE_CHANNEL",
-//             left: compile_time_environment_position(compile_environment, component.left.sym),
-//
-//         };
-//     } else if (component.tag === undefined) {
-//         // Do nothing
-//     } else if (component.tag === "EmptyStatement") {
-//         throw new TypeError(`Empty statements are not allowed in this compiler`);
-//     } else {
-//         throw new TypeError(`Unknown construct: ${component.tag}`);
-//     }
-// }
+let INSTRUCTIONS: any[] = [];
+let wc: number = 0;
+
+function compile_component(component: any, compile_environment: any[]): void {
+    if (component.tag === "lit") {
+        INSTRUCTIONS[wc++] = { tag: "LDC", val: component.val };
+    } else if (component.tag === "binop") {
+        compile_component(component.frst, compile_environment);
+        compile_component(component.scnd, compile_environment);
+        INSTRUCTIONS[wc++] = { tag: "BINOP", sym: component.sym };
+    } else if (component.tag === "unop") {
+        compile_component(component.frst, compile_environment);
+        INSTRUCTIONS[wc++] = { tag: "UNOP", sym: component.sym };
+    } else if (component.tag === "seq") {
+        const sequence = component.stmts;
+        if (sequence.length === 0) {
+            INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
+        }
+        let frst = true;
+        for (let seq_part of sequence) {
+            frst ? frst = false : INSTRUCTIONS[wc++] = { tag: "POP" };
+            compile_component(seq_part, compile_environment);
+        }
+    } else if (component.tag === "nam") {
+        INSTRUCTIONS[wc++] = {
+            tag: "LD",
+            sym: component.sym,
+            pos: compile_time_environment_position(compile_environment, component.sym)
+        };
+    } else if (component.tag === "assmt") {
+        compile_component(component.right, compile_environment);
+        INSTRUCTIONS[wc++] = {
+            tag: "ASSIGN",
+            pos: compile_time_environment_position(compile_environment, component.left.sym)
+        };
+    } else if (component.tag === "cond"){
+        compile_component(component.pred, compile_environment);
+        const jump_on_false_instr: { tag: string, addr?: number } = { tag: "JOF" };
+        INSTRUCTIONS[wc++] = jump_on_false_instr;
+        const alternative_address = wc;
+        compile_component(component.cons, compile_environment);
+        const goto_instr: { tag: string, addr?: number } = { tag: "GOTO" };
+        INSTRUCTIONS[wc++] = goto_instr;
+        compile_component(component.alt, compile_environment);
+        jump_on_false_instr.addr = alternative_address;
+        goto_instr.addr = wc;
+    } else if (component.tag === "app") { // Changed from 'args' to 'arguments' to match parser
+        compile_component(component.fun, compile_environment);
+        const args = component.arguments;
+        for (let arg of args) {
+            compile_component(arg, compile_environment);
+        }
+        INSTRUCTIONS[wc++] = { tag: "CALL", arity: args.length };
+    } else if (component.tag === "blk") {
+        const locals = scan(component.body);
+        INSTRUCTIONS[wc++] = { tag: "ENTER_SCOPE", num: locals.length, syms: locals };
+        compile_component(component.body, compile_time_environment_extend(locals, compile_environment));
+        INSTRUCTIONS[wc++] = { tag: "EXIT_SCOPE" };
+    } else if (component.tag === "const") {
+        const pos = compile_time_environment_position(compile_environment, component.sym);
+        if (is_null(component.expr)) {
+            INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
+        } else {
+            compile_component(component.expr, compile_environment);
+        }
+        INSTRUCTIONS[wc++] = {
+            tag: "ASSIGN",
+            pos: pos
+        };
+    } else if (component.tag === "var") {
+        const pos = compile_time_environment_position(compile_environment, component.sym);
+        if (is_null(component.expr)) {
+            INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
+        } else {
+            compile_component(component.expr, compile_environment);
+        }
+        INSTRUCTIONS[wc++] = {
+            tag: "ASSIGN",
+            pos: pos
+        };
+    } else if (component.tag === "ReturnStatement") { // Changed from 'ret' and 'component.argument' to match parser
+        compile_component(component.argument, compile_environment);
+        INSTRUCTIONS[wc++] = { tag: "RESET" };
+    } else if (component.tag === "fun") {
+        // Rewrite as a const declaration to a lambda function
+        compile_component({
+            tag: "const",
+            sym: component.sym,
+            expr: {
+                tag: "lam",
+                prms: component.prms,
+                arity: component.prms.length,
+                body: component.body
+            }
+        }, compile_environment);
+    } else if (component.tag === "lam") {
+        INSTRUCTIONS[wc++] = { tag: "LDF", arity: component.arity, addr: wc + 1 };
+        const goto_instr: { tag: string, addr?: number } = { tag: "GOTO" };
+        INSTRUCTIONS[wc++] = goto_instr;
+        compile_component(component.body, compile_time_environment_extend(component.prms, compile_environment));
+        INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
+        INSTRUCTIONS[wc++] = { tag: "RESET" };
+        goto_instr.addr = wc;
+    } else if (component.tag === "goroutine") {
+        // Compile application call
+        compile_component(component.function, compile_environment);
+        // Change "CALL" to "GOCALL"
+        INSTRUCTIONS[wc - 1] = { tag: "GOCALL", arity: INSTRUCTIONS[wc - 1].arity };
+        INSTRUCTIONS[wc++] = { tag: "LDC", val: undefined };
+    } else if (component.tag === "MakeChannel") {
+        INSTRUCTIONS[wc++] = { tag: "CREATE_CHAN" };
+    } else if (component.tag === "Arrow") {
+        compile_component(component.right, compile_environment);
+        INSTRUCTIONS[wc++] = {
+            tag: "USE_CHANNEL",
+            left: compile_time_environment_position(compile_environment, component.left.sym),
+
+        };
+    } else if (component.tag === undefined) {
+        // Do nothing
+    } else if (component.tag === "EmptyStatement") {
+        throw new TypeError(`Empty statements are not allowed in this compiler`);
+    } else {
+        throw new TypeError(`Unknown construct: ${component.tag}`);
+    }
+}
 
 /* ================ COMPILER ENVIRONMENT ================ */
 
@@ -418,14 +418,21 @@ function initSystem() {
 }
 
 
-
-
 /* === Test Cases === */
 const test_addition = {
     "tag": "BINOP",
     "sym": "+",
     "operands": [
-        {"tag": "LIT", "val": 1},  // First operand as a literal value 1
-        {"tag": "LIT", "val": 0}   // Second operand as a literal value 2
+        {"tag": "LIT", "val": 3},  // First operand as a literal value 1
+        {"tag": "LIT", "val": 3}   // Second operand as a literal value 2
     ]
 };
+
+function executeTestCase(testCase: any) {
+    const { sym, operands } = testCase;
+    const result = binop_microcode[sym](operands[0].val, operands[1].val);
+    console.log(`Test Case Result: ${result}`);
+}
+
+/* === Run Test Case === */
+executeTestCase(test_addition);
